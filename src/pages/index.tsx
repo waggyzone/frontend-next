25715e3e77c778d5ee037fc0d9dd887e35a79041
hellosudhiim <hellosudhii@gmail.com>
2023-02-11 14:35:38 +0530

    feat: added home page and section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,8 @@ export default function Home() {
     const session = await getSession();
     console.log(session);
   })();
+
+  const inner_html = () => <img src={`/Images/banner.png`}/>
   return (
     <>
       <Head>
@@ -17,10 +19,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className=" w-screen h-screen pt-16 flex-1  flex  flex-row overflow-auto">
-        <div className="bg-red-500 w-screen"><img src="/Image/Dog.png"></img>
+      <div className=" w-screen h-screen pt-16 flex-1  flex  flex-row overflow-auto bg-no-repeat">
+        <section id="home">
+            <div className="w-screen flex justifiy-between items-center bg-no-repeat "
+          style={{
+         backgroundImage:`url(/Images/banner.png)`,
+       }}
+        >
+          <div className="w-[300px]">
+
+            <p>Theertha</p>
+          </div>
+          <div className="  h-fit w-fit bottom-1 mt-[20rem]  ">
+            <img src={`/Images/Dog.png`} className="w-[65rem] h-[50rem] "/>
+          </div>
         </div>
-       
+      </section>
+        <section id="#1">
+          <h1>service for every dog</h1>
+       </section>
        
       </div>
     </>
