7d54eecb76a290c18592dc34fbb219dfb8a9c407
hellosudhiim <hellosudhii@gmail.com>
2023-04-24 22:48:26 +0530

    feat: pages added

---
+++ src/pages/daycare/index.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps, NextPage } from "next";
+
+const Daycare: NextPage = () => {
+  return <div>Enter</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {},
+  };
+};
+
+export default Daycare;
---
+++ src/pages/training/index.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps, NextPage } from "next";
+
+const Training: NextPage = () => {
+  return <div>Enter</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {},
+  };
+};
+
+export default Training;
